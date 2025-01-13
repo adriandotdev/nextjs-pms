288483ee802ede700503daad34f1909d4e23ea18
Adrian Nads Marcelo <marcelonads@gmail.com>
2025-01-13 23:51:58 +0800

    development: Added prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 "use client";
+import { useEffect } from "react";
 import { trpc } from "./_trpc/client";
 
 export default function Home() {
+	const result = trpc.getProducts.useQuery();
+
+	console.log(result.data);
+
+	const addTodo = trpc.addProduct.useMutation({
+		onSettled: () => {
+			result.refetch();
+		},
+	});
+
+	useEffect(() => {
+		addTodo.mutate({
+			name: "Shayna",
+			password: "password",
+		});
+	}, []);
 	return (
 		<>
 			<h1>Hello to NextJS and tRPC</h1>
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,26 @@
 import { publicProcedure, router } from "./trpc";
 
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+import { z } from "zod";
 export const appRouter = router({
 	getProducts: publicProcedure.query(async () => {
-		return [];
+		const result = await prisma.user.findMany();
+
+		return result;
 	}),
+	addProduct: publicProcedure
+		.input(z.object({ name: z.string(), password: z.string() }))
+		.mutation(async (opts) => {
+			await prisma.user.create({
+				data: {
+					name: opts.input.name,
+					password: opts.input.password,
+				},
+			});
+		}),
 });
 
 export type AppRouter = typeof appRouter;
